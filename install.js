4a807849cced90013d8a5e4a78e0e3cf58b3e100
Michal Budzynski <michal@virtualdesign.pl>
2014-07-22 19:34:34 +0200

    package.json generator moved to separate module

--- install.js
+++ install.js
@@ -1,39 +1,11 @@
 'use strict';
 
-var exec = require('child_process').exec;
-var async = require('async');
-var fs = require('fs');
+var pjg = require('./scripts/package-json-generator');
 
-var packageJsonStructure = {
-  name: "",
-  version: "0.0.1",
-  author: "",
-  repository: ''
-}
-
-
-var parseGitCommand = function(instruction, cb) {
-  var key = Object.keys(instruction)[0];
-  var command = instruction[key];
-
-  exec(command, function(error, stdout, stderr) {
-    packageJsonStructure[key] = stdout.trim();
-    cb();
-  });
-}
-
-var gitConfigCommands = [
-  { name: "(basename $(git rev-parse --show-toplevel))" },
-  { author: "git log --all --format='%aN <%cE>' | sort -u | head -1" },
-  { repository: "git config --get remote.origin.url" }
-];
-
-async.each(gitConfigCommands, parseGitCommand, function() {
-  fs.writeFile("package.json", JSON.stringify(packageJsonStructure, null, 2), function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("package.json file saved...");
-    }
-  });
-});
+pjg(function(err, msg) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(msg);
+})
