f66a2986850df4cb0b9217a07fdfcfe246147e02
Michal Budzynski <michal@virtualdesign.pl>
2014-07-23 02:09:25 +0200

    [MICHAL] - :rocket: fancom config added

---
+++ scripts/fancom-generator.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var https = require('https');
+var fs = require('fs');
+var config = 'https://raw.githubusercontent.com/michalbe/fancom/master/.fancom';
+
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(function(){
+        cb(null, 'Fancom config downloaded');
+      });
+    });
+  });
+};
+
+module.exports = function(callback){
+  download(config, '.fancom', callback);
+};
