bbcea24c785f70c38972e24aa5f87007d61816bb
Michal Budzynski <michal@virtualdesign.pl>
2014-07-24 18:42:31 +0200

    [MICHAL] - cli defined

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,18 @@
+#!/usr/bin/env node
 'use strict';
 
 var async = require('async');
 var tasks = [
-  require('./scripts/precommit-hook'),
-  require('./scripts/jshintrc'),
-  require('./scripts/fancom'),
-  require('./scripts/tests'),
-  require('./scripts/assert')
+  require('../scripts/precommit-hook'),
+  require('../scripts/jshintrc'),
+  require('../scripts/fancom'),
+  require('../scripts/tests'),
+  require('../scripts/assert')
 ];
 
-require('./scripts/package-json')(function(err, msg) {
+// I know this carbonara code is awful, but it's 05:27AM
+// and my last cigarette break was around 18:30
+require('../scripts/package-json')(function(err, msg) {
   if (err) {
     console.log(err);
     return;
@@ -28,7 +31,7 @@ require('./scripts/package-json')(function(err, msg) {
       });
     },
     function() {
-      require('./scripts/scripts')(function(err, msg){
+      require('../scripts/scripts')(function(err, msg){
         if (err) {
           console.log(err);
           return;
